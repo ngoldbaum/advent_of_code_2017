41f8485915fecce5cd15f621fd9e5c5ea7911944
Nathan Goldbaum <ngoldbau@illinois.edu>
2017-12-08 13:34:14 -0600

    first half of day 8

---
+++ day_8/registers.py
@@ -0,0 +1,31 @@
+import operator
+
+DISPATCH_TABLE = {
+    'inc': operator.add,
+    'dec': operator.sub,
+    '>': operator.gt,
+    '>=': operator.ge,
+    '<': operator.lt,
+    '<=': operator.le,
+    '==': operator.eq,
+    '!=': operator.ne,
+}
+
+
+if __name__ == "__main__":
+    with open('input', 'r') as f:
+        text = f.readlines()
+
+    instructions = [t.strip().split() for t in text]
+
+    registers = set([i[0] for i in instructions])
+
+    state = {}
+    for r in registers:
+        state[r] = 0
+
+    for i in instructions:
+        if DISPATCH_TABLE[i[5]](state[i[4]], int(i[6])):
+            state[i[0]] = DISPATCH_TABLE[i[1]](state[i[0]], int(i[2]))
+
+    print(max(state.values()))
